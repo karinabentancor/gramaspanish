c6dbca8f373c90eb5578b4c14d6e579d185dd01a
karinabentancor <karinabentancorlenzi@gmail.com>
2025-03-04 20:11:34 -0300

    testimonials-update

--- js/script.js
+++ js/script.js
@@ -1,31 +1,64 @@
 function loadHTML(id, filename) {
-        fetch(filename)
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById(id).innerHTML = data;
-            });
+    fetch(filename)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        });
+}
+
+
+loadHTML('navbar', 'base.html');
+loadHTML('footer', 'base.html');
+
+
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+document.querySelectorAll(".navbar ul li a").forEach(link => {
+    if (link.href === window.location.href) {
+        link.classList.add("active");
     }
+});
 
- 
-    loadHTML('navbar', 'base.html');
-    loadHTML('footer', 'base.html');
+function loadHTML(id, filename) {
+    fetch(filename)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        });
+}
 
+loadHTML('navbar', 'base.html');
+loadHTML('footer', 'base.html');
 
-    var swiper = new Swiper('.swiper-container', {
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
 
-
-    document.querySelectorAll(".navbar ul li a").forEach(link => {
-        if (link.href === window.location.href) {
-            link.classList.add("active");
-        }
-    });
+document.querySelectorAll(".navbar ul li a").forEach(link => {
+    if (link.href === window.location.href) {
+        link.classList.add("active");
+    }
+});
